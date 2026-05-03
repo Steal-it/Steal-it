a121ce146d143ffdf8c5552843409b28a0584fb1
Soranzo Andrea <andreajsm03@gmail.com>
2026-05-03 23:39:02 +0200

    added FBF to AfterImagePass
    still does not work

--- Assets/Shaders/Fullscreen/CustomPasses/AfterImage/AfterImageRenderFeature.cs
+++ Assets/Shaders/Fullscreen/CustomPasses/AfterImage/AfterImageRenderFeature.cs
@@ -9,7 +9,7 @@ public class AfterImageRenderFeature : ScriptableRendererFeature
 {
     [SerializeField]
     private AfterImageSettings settings;
-    AfterImagePass m_ScriptablePass;
+    AfterImagePass afterImagePass;
 
     [SerializeField]
     private RenderPassEvent renderEvent = RenderPassEvent.AfterRenderingPostProcessing;
@@ -17,43 +17,48 @@ public class AfterImageRenderFeature : ScriptableRendererFeature
     // called on render feature activation and deactivation
     public override void Create()
     {
-        if (settings.afterImageMaterial == null) return;
+        if (settings.material == null) return;
 
-        m_ScriptablePass = new AfterImagePass(settings);
+        settings.material.SetInt("_UseFBF", settings.useFBF ? 1 : 0);
+        settings.material.SetInt("_UseGrayscale", settings.useGrayscale ? 1 : 0);
+
+        afterImagePass = new AfterImagePass(settings);
 
         // Configures where the render pass should be injected.
-        m_ScriptablePass.renderPassEvent = renderEvent;
+        afterImagePass.renderPassEvent = renderEvent;
 
         // You can request URP color texture and depth buffer as inputs by uncommenting the line below,
         // URP will ensure copies of these resources are available for sampling before executing the render pass.
         // Only uncomment it if necessary, it will have a performance impact, especially on mobiles and other TBDR GPUs where it will break render passes.
-        m_ScriptablePass.ConfigureInput(ScriptableRenderPassInput.Color);
+        afterImagePass.ConfigureInput(ScriptableRenderPassInput.Color);
 
         // You can request URP to render to an intermediate texture by uncommenting the line below.
         // Use this option for passes that do not support rendering directly to the backbuffer.
         // Only uncomment it if necessary, it will have a performance impact, especially on mobiles and other TBDR GPUs where it will break render passes.
-        m_ScriptablePass.requiresIntermediateTexture = true;
+        afterImagePass.requiresIntermediateTexture = true;
     }
 
     // Here you can inject one or multiple render passes in the renderer.
     // This method is called when setting up the renderer once per-camera.
     public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
     {
-        if (settings.afterImageMaterial == null || renderingData.cameraData.cameraType != CameraType.Game) return;
-        renderer.EnqueuePass(m_ScriptablePass);
+        if (settings.material == null || renderingData.cameraData.cameraType != CameraType.Game) return;
+        renderer.EnqueuePass(afterImagePass);
     }
 
     // called when the game stops
     protected override void Dispose(bool disposing)
     {
-        m_ScriptablePass?.Dispose();
+        afterImagePass?.Dispose();
     }
 
     // Use this class to pass around settings from the feature to the pass
     [Serializable]
     public class AfterImageSettings
     {
-        public Material afterImageMaterial;
+        public Material material;
+        public bool useFBF;
+        public bool useGrayscale;
     }
 
     class AfterImagePass : ScriptableRenderPass
@@ -73,110 +78,48 @@ public class AfterImageRenderFeature : ScriptableRendererFeature
         {
             internal Material material;
             internal float blend;
-            internal TextureHandle activeColor;
-            internal TextureHandle historyTexture;
+            internal TextureHandle activeColorTexture;
         }
 
-        // RecordRenderGraph is where the RenderGraph handle can be accessed, through which render passes can be added to the graph.
-        // FrameData is a context container through which URP resources can be accessed and managed.
-        // public override void RecordRenderGraph(RenderGraph renderGraph, ContextContainer frameData)
-        // {
-        //     VolumeStack stack = VolumeManager.instance.stack;
-        //     AfterImageEffect effect = stack.GetComponent<AfterImageEffect>();
-
-        //     if (!effect.IsActive())
-        //     {
-        //         return;
-        //     }
-
-        //     float blendAmount = effect.GetValue();
-
-        //     // UniversalResourceData contains all the texture handles used by the renderer, including the active color and depth textures
-        //     // The active color and depth textures are the main color and depth buffers that the camera renders into
-        //     UniversalResourceData resourceData = frameData.Get<UniversalResourceData>();
-
-        //     // This should never happen since we set m_Pass.requiresIntermediateTexture = true;
-        //     // Unless you set the render event to AfterRendering, where we only have the BackBuffer. 
-        //     if (resourceData.isActiveTargetBackBuffer)
-        //     {
-        //         Debug.LogError($"Skipping render pass. AfterImageRenderFeature requires an intermediate ColorTexture, we can't use the BackBuffer as a texture input.");
-        //         return;
-        //     }
-
-        //     if (isFirstFrame)
-        //     {
-        //         last_source = resourceData.activeColorTexture;      // read the frame on first frame in order to avoid blackscreen
-        //         isFirstFrame = false;
-        //     }
-
-        //     TextureHandle source = resourceData.activeColorTexture;
-
-        //     settings.afterImageMaterial.SetFloat("_BlendAmount", blendAmount);
-        //     settings.afterImageMaterial.SetTexture("_HistoryTex", last_source);
-
-        //     TextureDesc destinationDesc = source.GetDescriptor(renderGraph);
-        //     destinationDesc.name = $"AfterImage-{_passName}";
-        //     destinationDesc.clearBuffer = false;
-
-        //     TextureHandle destination = renderGraph.CreateTexture(destinationDesc);
-
-
-        //     RenderGraphUtils.BlitMaterialParameters temp_para = new(source, temp_source, settings.afterImageMaterial, 0);          // save the effect on the temp
-        //     RenderGraphUtils.BlitMaterialParameters save_para = new(temp_source, last_source, settings.afterImageMaterial, 0);     // save the temp for the next iteration
-        //     RenderGraphUtils.BlitMaterialParameters apply_para = new(temp_source, destination, settings.afterImageMaterial, 0);    // apply the effect on the current frame
-
-        //     renderGraph.AddBlitPass(temp_para, passName: _passName);
-        //     // renderGraph.AddBlitPass(save_para, passName: _passName);
-        //     // renderGraph.AddBlitPass(apply_para, passName: _passName);
-
-        //     // FrameData allows to get and set internal pipeline buffers. Here we update the CameraColorBuffer to the texture that we just wrote to in this pass. 
-        //     // Because RenderGraph manages the pipeline resources and dependencies, following up passes will correctly use the right color buffer.
-        //     // This optimization has some caveats. You have to be careful when the color buffer is persistent across frames and between different cameras, such as in camera stacking.
-        //     // In those cases you need to make sure your texture is an RTHandle and that you properly manage the lifecycle of it.
-        //     resourceData.cameraColor = destination;
-
-        // }
 
         // This static method is passed as the RenderFunc delegate to the RenderGraph render pass.
         // It is used to execute draw commands.
-
-        //
-
         // What the GPU execute
         static void ExecutePass(PassData data, RasterGraphContext context)
         {
-            if (data.historyTexture.IsValid())
-            {
-                data.material.SetTexture("_HistoryTex", data.historyTexture);
-                data.material.SetFloat("_BlendAmount", data.blend);
-
-                // To blit by appling the material using the default scaleBias
-                Blitter.BlitTexture(context.cmd, data.activeColor, new Vector4(1, 1, 0, 0), data.material, 0);
-            }
+            Blitter.BlitTexture(context.cmd, data.activeColorTexture, new Vector4(1, 1, 0, 0), data.material, 0);
         }
 
         // RecordRenderGraph is where the RenderGraph handle can be accessed, through which render passes can be added to the graph.
         // FrameData is a context container through which URP resources can be accessed and managed.
         public override void RecordRenderGraph(RenderGraph renderGraph, ContextContainer frameContext)
         {
+
             VolumeStack stack = VolumeManager.instance.stack;
             AfterImageEffect effect = stack.GetComponent<AfterImageEffect>();
-            if (!effect.IsActive()) return;
 
-            float blendAmount = effect.GetIntensity();
-
-            UniversalResourceData resourceData = frameContext.Get<UniversalResourceData>();
-
-            if (resourceData.isActiveTargetBackBuffer) // cant read form the backbuffer (the actual buffer of the screen)
+            if (!effect.IsActive())
             {
-                Debug.LogError("AfterImageRenderFeature requires an intermediate ColorTexture.");
                 return;
             }
 
-            // get the current screen
+            float blendAmount = effect.GetIntensity();
+
+            // UniversalResourceData contains all the texture handles used by the renderer, including the active color and depth textures
+            // The active color and depth textures are the main color and depth buffers that the camera renders into
+            UniversalResourceData resourceData = frameContext.Get<UniversalResourceData>();
+
+            // This should never happen since we set m_Pass.requiresIntermediateTexture = true;
+            // Unless you set the render event to AfterRendering, where we only have the BackBuffer. 
+            if (resourceData.isActiveTargetBackBuffer)
+            {
+                Debug.LogError($"Skipping render pass. AfterImageRenderFeature requires an intermediate ColorTexture, we can't use the BackBuffer as a texture input.");
+                return;
+            }
+
             TextureHandle source = resourceData.activeColorTexture;
 
-            // grab info (description) of the screen
+            //grab info(description) of the screen
             TextureDesc desc = renderGraph.GetTextureDesc(source);
 
             // Allocate persistent history RTHandle if needed or if resolution changed
@@ -190,6 +133,7 @@ public class AfterImageRenderFeature : ScriptableRendererFeature
                 );
             }
 
+            // this is needed to that copy to the RTHandle
             TextureHandle historyHandle = renderGraph.ImportTexture(m_HistoryRT);
 
             if (isFirstFrame)
@@ -201,36 +145,70 @@ public class AfterImageRenderFeature : ScriptableRendererFeature
                 return; // skip blend on first frame
             }
 
-            // Create destination for this frame's output
-            desc.name = "AfterImage_Destination";
-            desc.clearBuffer = false;
-            TextureHandle destination = renderGraph.CreateTexture(desc);
+            TextureDesc destinationDesc = source.GetDescriptor(renderGraph);
+            destinationDesc.name = $"AfterImage_{_passName}";
+            destinationDesc.clearBuffer = false;
 
+            TextureHandle destination = renderGraph.CreateTexture(destinationDesc);
+
+            settings.material.SetFloat("_BlendAmount", blendAmount);
+            // I must use the RTHandle because the TextureHandle can be access on RenderFunc where it will be populated
+            settings.material.SetTexture("_HistoryTex", m_HistoryRT);
+
+            if (settings.useFBF)
+            {
+                FBFPass(renderGraph, frameContext, source, destination, historyHandle, blendAmount);
+            }
+            else
+            {
+                NormalPass(renderGraph, frameContext, source, destination, historyHandle, blendAmount);
+            }
+
+            // FrameData allows to get and set internal pipeline buffers. Here we update the CameraColorBuffer to the texture that we just wrote to in this pass. 
+            // Because RenderGraph manages the pipeline resources and dependencies, following up passes will correctly use the right color buffer.
+            // This optimization has some caveats. You have to be careful when the color buffer is persistent across frames and between different cameras, such as in camera stacking.
+            // In those cases you need to make sure your texture is an RTHandle and that you properly manage the lifecycle of it.
+            resourceData.cameraColor = destination;
+            // renderGraph.AddCopyPass(destination, source, passName: "Copy Back FF Destination (also using FBF)"); //?????????
+
+
+        }
+
+        private void NormalPass(RenderGraph renderGraph, ContextContainer frameContext, TextureHandle source, TextureHandle destination, TextureHandle history, float blendAmount)
+        {
+            RenderGraphUtils.BlitMaterialParameters para = new(source, destination, settings.material, 0);    // apply the effect on the current frame
+
+            // Shortcut for the RasterRenderPass
+            renderGraph.AddBlitPass(para, passName: _passName);
+
+            // update the RTHandle 
+            renderGraph.AddCopyPass(destination, history, passName: "AfterImage_SaveHistory");
+        }
+
+        private void FBFPass(RenderGraph renderGraph, ContextContainer frameContext, TextureHandle source, TextureHandle destination, TextureHandle history, float blendAmount)
+        {
             // render pass (GPU job) 
             using (var builder = renderGraph.AddRasterRenderPass<PassData>(_passName, out var passData))
             {
                 // populating data
-                passData.material = settings.afterImageMaterial;
+                passData.material = settings.material;
                 passData.blend = blendAmount;
-                passData.activeColor = source;
-                passData.historyTexture = historyHandle;
+                passData.activeColorTexture = source;
 
-                // use those tex in readonly
-                builder.UseTexture(source, AccessFlags.Read);
-                builder.UseTexture(historyHandle, AccessFlags.Read);
+                // We declare the src as input attachment. This is required for framebuffer fetch. 
+                builder.SetInputAttachment(source, 0, AccessFlags.Read);
+                // builder.UseTexture(source, AccessFlags.Read);
 
-                // set the final destination if the process
+                // set the final destination of the frame.
+                // We cannot use MRT because shader_graph does not support it
                 builder.SetRenderAttachment(destination, 0);
+                // builder.SetRenderAttachment(history, 1);
 
                 // assign the task
                 builder.SetRenderFunc((PassData data, RasterGraphContext context) => ExecutePass(data, context));
             }
-
-            // Save destination into history for NEXT frame (before reassigning cameraColor)
-            renderGraph.AddCopyPass(destination, historyHandle, passName: "AfterImage_SaveHistory");
-
-            // Now redirect camera output
-            resourceData.cameraColor = destination;
+            // update the RTHandle 
+            renderGraph.AddCopyPass(destination, history, passName: "AfterImage_SaveHistory");
         }
 
         public void Dispose()
