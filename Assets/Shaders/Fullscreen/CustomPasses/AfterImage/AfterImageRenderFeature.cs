0e3404629164ac1ba6617318d93c0609e8a5f6f6
Soranzo Andrea <andreajsm03@gmail.com>
2026-05-04 22:27:08 +0200

    AfterImageRenderFeature cleaning

--- Assets/Shaders/Fullscreen/CustomPasses/AfterImage/AfterImageRenderFeature.cs
+++ Assets/Shaders/Fullscreen/CustomPasses/AfterImage/AfterImageRenderFeature.cs
@@ -4,20 +4,17 @@ using UnityEngine.Rendering;
 using UnityEngine.Rendering.Universal;
 using UnityEngine.Rendering.RenderGraphModule;
 using UnityEngine.Rendering.RenderGraphModule.Util;
-using UnityEditor.EditorTools;
 
-public class AfterImageRenderFeature : ScriptableRendererFeature
-{
+public class AfterImageRenderFeature : ScriptableRendererFeature {
     [SerializeField]
     private AfterImageSettings settings;
-    AfterImagePass afterImagePass;
+    private AfterImagePass afterImagePass;
 
     [SerializeField]
     private RenderPassEvent renderEvent = RenderPassEvent.AfterRenderingPostProcessing;
 
     // called on render feature activation and deactivation
-    public override void Create()
-    {
+    public override void Create() {
         if (settings.material == null) return;
 
         settings.material.SetInt("_UseFBF", settings.useFBF ? 1 : 0);
@@ -34,6 +31,12 @@ public class AfterImageRenderFeature : ScriptableRendererFeature
         //     settings.material.EnableKeyword("_USEGRAYSCALE_ON");
         // else
         //     settings.material.DisableKeyword("_USEGRAYSCALE_ON");
+
+        // if (settings.badGrayscale)
+        //     settings.material.EnableKeyword("_BADGRAYSCALE_ON");
+        // else
+        //     settings.material.DisableKeyword("_BADGRAYSCALE_ON");
+
 
         afterImagePass = new AfterImagePass(settings);
 
@@ -53,79 +56,65 @@ public class AfterImageRenderFeature : ScriptableRendererFeature
 
     // Here you can inject one or multiple render passes in the renderer.
     // This method is called when setting up the renderer once per-camera.
-    public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
-    {
+    public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData) {
         if (settings.material == null || renderingData.cameraData.cameraType != CameraType.Game) return;
         renderer.EnqueuePass(afterImagePass);
     }
 
     // called when the game stops
-    protected override void Dispose(bool disposing)
-    {
+    protected override void Dispose(bool disposing) {
         afterImagePass?.Dispose();
     }
 
     // Use this class to pass around settings from the feature to the pass
     [Serializable]
-    public class AfterImageSettings
-    {
+    public class AfterImageSettings {
         public Material material;
         [Tooltip("Turn on frame buffer fetch for mobile optimization")]
         public bool useFBF;
         public bool useGrayscale;
-        [Tooltip("Useful for after flash effect")]
+        [Tooltip("Useful for after flash effect (it requires the useGrayscale)")]
         public bool badGrayscale;
     }
 
-    class AfterImagePass : ScriptableRenderPass
-    {
-        private const string _passName = "AfterImagePass";
+    class AfterImagePass : ScriptableRenderPass {
+        private const string _passName = "AfterImage";
 
         readonly AfterImageSettings settings;
+
         private RTHandle m_HistoryRT;
         private bool isFirstFrame = true;
 
-        public AfterImagePass(AfterImageSettings settings)
-        {
+        public AfterImagePass(AfterImageSettings settings) {
             this.settings = settings;
         }
 
-        private class PassData
-        {
+        private class PassData {
             internal Material material;
             internal bool usingFBF;
             internal TextureHandle activeColorTexture;
         }
 
-
         // This static method is passed as the RenderFunc delegate to the RenderGraph render pass.
         // It is used to execute draw commands.
         // What the GPU execute
-        static void ExecutePass(PassData data, RasterGraphContext context)
-        {
-            if (data.usingFBF)
-            {
+        static void ExecutePass(PassData data, RasterGraphContext context) {
+            if (data.usingFBF) {
                 // dont need to specify the source in FBF because is well known
                 Blitter.BlitTexture(context.cmd, new Vector4(1, 1, 0, 0), data.material, 0);
-            }
-            else
-            {
+            } else {
                 Blitter.BlitTexture(context.cmd, data.activeColorTexture, new Vector4(1, 1, 0, 0), data.material, 0);
             }
         }
 
         // RecordRenderGraph is where the RenderGraph handle can be accessed, through which render passes can be added to the graph.
         // FrameData is a context container through which URP resources can be accessed and managed.
-        public override void RecordRenderGraph(RenderGraph renderGraph, ContextContainer frameContext)
-        {
+        public override void RecordRenderGraph(RenderGraph renderGraph, ContextContainer frameContext) {
 
             VolumeStack stack = VolumeManager.instance.stack;
             AfterImageEffect effect = stack.GetComponent<AfterImageEffect>();
 
-            if (!effect.IsActive())
-            {
-                return;
-            }
+            if (!effect.IsActive()) return;
 
             float blendAmount = effect.GetIntensity();
 
@@ -135,56 +124,48 @@ public class AfterImageRenderFeature : ScriptableRendererFeature
 
             // This should never happen since we set m_Pass.requiresIntermediateTexture = true;
             // Unless you set the render event to AfterRendering, where we only have the BackBuffer. 
-            if (resourceData.isActiveTargetBackBuffer)
-            {
+            if (resourceData.isActiveTargetBackBuffer) {
                 Debug.LogError($"Skipping render pass. AfterImageRenderFeature requires an intermediate ColorTexture, we can't use the BackBuffer as a texture input.");
                 return;
             }
 
             TextureHandle source = resourceData.activeColorTexture;
 
-            //grab info(description) of the screen
+            // grab info(description) of the screen
             TextureDesc desc = renderGraph.GetTextureDesc(source);
 
             // Allocate persistent history RTHandle if needed or if resolution changed
-            if (m_HistoryRT == null || m_HistoryRT.rt.width != desc.width || m_HistoryRT.rt.height != desc.height)
-            {
+            if (m_HistoryRT == null || m_HistoryRT.rt.width != desc.width || m_HistoryRT.rt.height != desc.height) {
                 m_HistoryRT?.Release();
                 m_HistoryRT = RTHandles.Alloc(
                     desc.width, desc.height,
                     colorFormat: desc.colorFormat,
-                    name: "AfterImage_History"
+                    name: $"{_passName}_History"
                 );
             }
 
             // this is needed to that copy to the RTHandle
             TextureHandle historyHandle = renderGraph.ImportTexture(m_HistoryRT);
 
-            if (isFirstFrame)
-            {
+            if (isFirstFrame) {
                 // Seed history with current frame so frame 1 isn't black
-                renderGraph.AddCopyPass(source, historyHandle, passName: "AfterImage_SeedHistory");
+                renderGraph.AddCopyPass(source, historyHandle, passName: $"{_passName}_SeedHistory");
                 resourceData.cameraColor = source;
                 isFirstFrame = false;
                 return; // skip blend on first frame
             }
 
             TextureDesc destinationDesc = source.GetDescriptor(renderGraph);
-            destinationDesc.name = $"AfterImage_{_passName}";
             destinationDesc.clearBuffer = false;
-
+            destinationDesc.name = $"{_passName}_Destination";
             TextureHandle destination = renderGraph.CreateTexture(destinationDesc);
 
             settings.material.SetFloat("_BlendAmount", blendAmount);
-            // I must use the RTHandle because the TextureHandle can be access on RenderFunc where it will be populated
-            settings.material.SetTexture("_HistoryTex", m_HistoryRT);
+            settings.material.SetTexture("_HistoryTex", m_HistoryRT); // I must use the RTHandle because the TextureHandle can be access on RenderFunc where it will be populated
 
-            if (settings.useFBF)
-            {
+            if (settings.useFBF) {
                 FBFPass(renderGraph, frameContext, source, destination, historyHandle, blendAmount);
-            }
-            else
-            {
+            } else {
                 NormalPass(renderGraph, frameContext, source, destination, historyHandle, blendAmount);
             }
 
@@ -198,12 +179,13 @@ public class AfterImageRenderFeature : ScriptableRendererFeature
 
         }
 
-        private void NormalPass(RenderGraph renderGraph, ContextContainer frameContext, TextureHandle source, TextureHandle destination, TextureHandle history, float blendAmount)
-        {
+        private void NormalPass(RenderGraph renderGraph, ContextContainer frameContext, TextureHandle source, TextureHandle destination, TextureHandle history, float blendAmount) {
+
+            const string passName = "_FBFPass";
+
             // render pass (GPU job) 
             // Even if is a simple blit operation i use the RasterRenderPass in order to merge passes since BlitPass is unsafe for some reason
-            using (var builder = renderGraph.AddRasterRenderPass<PassData>(_passName, out var passData))
-            {
+            using (var builder = renderGraph.AddRasterRenderPass<PassData>($"{_passName}_{passName}", out var passData)) {
                 // populating data
                 passData.material = settings.material;
                 passData.usingFBF = settings.useFBF;
@@ -222,14 +204,15 @@ public class AfterImageRenderFeature : ScriptableRendererFeature
                 builder.SetRenderFunc((PassData data, RasterGraphContext context) => ExecutePass(data, context));
             }
             // update the RTHandle 
-            renderGraph.AddCopyPass(destination, history, passName: "AfterImage_SaveHistory");
+            renderGraph.AddCopyPass(destination, history, passName: $"{_passName}_SaveHistory");
         }
 
-        private void FBFPass(RenderGraph renderGraph, ContextContainer frameContext, TextureHandle source, TextureHandle destination, TextureHandle history, float blendAmount)
-        {
+        private void FBFPass(RenderGraph renderGraph, ContextContainer frameContext, TextureHandle source, TextureHandle destination, TextureHandle history, float blendAmount) {
+
+            const string passName = "_NormalPass";
+
             // render pass (GPU job) 
-            using (var builder = renderGraph.AddRasterRenderPass<PassData>(_passName, out var passData))
-            {
+            using (var builder = renderGraph.AddRasterRenderPass<PassData>($"{_passName}_{passName}", out var passData)) {
                 // populating data
                 passData.material = settings.material;
                 passData.usingFBF = settings.useFBF;
@@ -248,11 +231,10 @@ public class AfterImageRenderFeature : ScriptableRendererFeature
                 builder.SetRenderFunc((PassData data, RasterGraphContext context) => ExecutePass(data, context));
             }
             // update the RTHandle 
-            renderGraph.AddCopyPass(destination, history, passName: "AfterImage_SaveHistory");
+            renderGraph.AddCopyPass(destination, history, passName: $"{_passName}_SaveHistory");
         }
 
-        public void Dispose()
-        {
+        public void Dispose() {
             m_HistoryRT?.Release();
         }
     }
