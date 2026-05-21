f1a618bf15f811fb64ae90f3054247e4f3eff8de
FireStoat3 <161834755+FireStoat3@users.noreply.github.com>
2026-05-21 20:10:15 +0200

    Added sender role to update object position

--- Assets/Scripts/Network/Spawner/SphereManager.cs
+++ Assets/Scripts/Network/Spawner/SphereManager.cs
@@ -9,6 +9,107 @@ using UnityEngine.XR.Interaction.Toolkit.Interactables;
 
 public class MovementMessage
 {
+    public Pose position;
+    public bool isOwned;
+}
+public class SphereManager : MonoBehaviour, INetworkSpawnable
+{
+    private Rigidbody body;
+    public NetworkId NetworkId {get;set;}
+
+    public bool owner;
+
+    public bool sender;
+
+    private NetworkContext context;
+    private XRGrabInteractable grabInteractable;
+
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody>();
+        grabInteractable = GetComponent<XRGrabInteractable>();
+        owner = false;
+    }
+
+    private void OnEnable()
+    {
+        grabInteractable.selectEntered.AddListener(OnGrab);
+        grabInteractable.selectExited.AddListener(OnRelease);
+    }
+
+    private void OnDisable()
+    {
+        grabInteractable.selectEntered.RemoveListener(OnGrab);
+        grabInteractable.selectExited.RemoveListener(OnRelease);
+    }
+
+    private void OnGrab(SelectEnterEventArgs args)
+    {
+        Debug.Log("Object grabbed by: " + args.interactorObject.transform.name);
+        owner = true;
+        sender = true;
+    }
+
+    private void OnRelease(SelectExitEventArgs args)
+    {
+        Debug.Log("Object released");
+        owner = false;
+        var msg = new MovementMessage();
+        msg.position = Transforms.ToLocal(transform,context.Scene.transform);
+        msg.isOwned = false;
+        context.SendJson(msg);
+    }
+
+    private void Start()
+    {
+        context = NetworkScene.Register(this);
+    }
+
+    private void SendMessage()
+    {
+        var message = new MovementMessage();
+        message.position = Transforms.ToLocal(transform,context.Scene.transform);
+        message.isOwned = owner;
+        context.SendJson(message);
+    }
+
+    private void FixedUpdate()
+    {
+        //Only sender transmit position, therefore, if I am not the sender I deactivate gravity
+        if(sender)
+        {
+            body.isKinematic = false;
+            SendMessage();
+        }
+        else
+        {
+            body.isKinematic = true;
+        }
+    }
+
+    public void ProcessMessage(ReferenceCountedSceneGraphMessage message)
+    {
+        var msg = message.FromJson<MovementMessage>();
+        var pose = Transforms.ToWorld(msg.position,context.Scene.transform);
+        transform.position = pose.position;
+        transform.rotation = pose.rotation;
+
+        if(msg.isOwned)
+        {
+            //If object is taken by another, The current player is no longer the sender
+            sender=false;
+            grabInteractable.enabled = false;
+        }
+        else
+        {
+            grabInteractable.enabled = true;
+        }
+    }
+}
+
+
+/*public class MovementMessage
+{
     public Pose pose;
     public bool free;
 }
@@ -104,4 +205,4 @@ public class SphereManager : MonoBehaviour, INetworkSpawnable
             grabInteractable.enabled = true;
         }
     }
-}
+}*/
