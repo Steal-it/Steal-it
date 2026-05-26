63981a8fa6a8a9a1406cbe13614b4e7e2008f137
Soranzo Andrea <andreajsm03@gmail.com>
2026-05-26 17:32:08 +0200

    right hand implemented

--- Assets/Scripts/Player/Torch/TorchControllerConfigurator.cs
+++ Assets/Scripts/Player/Torch/TorchControllerConfigurator.cs
@@ -1,17 +1,26 @@
 using System;
+using Unity.XR.CoreUtils;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.XR.Interaction.Toolkit.Interactors;
 
 public class TorchControllerConfigurator : MonoBehaviour, ITorchControllerConfigurator {
+
+    private enum Side {
+        Left,
+        Right
+    }
+
     public Transform HandTorchControllerTransform => handTorchController.transform;
 
     [SerializeField]
+    private Side side;
+    [SerializeField]
     private HandTorchController handTorchController;
     [SerializeField]
-    private NearFarInteractor nearFarInteractor;
-    [SerializeField]
     private InputActionReference controllerActivateInputAction;
+
+    private NearFarInteractor nearFarInteractor;
 
     public void ConfigureTorchHandController() {
         handTorchController.Configure(this);
@@ -26,6 +35,15 @@ public class TorchControllerConfigurator : MonoBehaviour, ITorchControllerConfig
         // TODO: track device change https://youtu.be/NObwdF9RqCg?si=QtYNqLSqe-xpNHuo
 
         handTorchController.Enable(_torchTransform);
+    }
+
+    private void Start() {
+        XROrigin origin = FindFirstObjectByType<XROrigin>();
+        if (side == Side.Left) {
+            nearFarInteractor = origin.transform.Find("Camera Offset/Left Controller").GetComponentInChildren<NearFarInteractor>();
+        } else {
+            nearFarInteractor = origin.transform.Find("Camera Offset/Right Controller").GetComponentInChildren<NearFarInteractor>();
+        }
     }
 
     public void Disable(Action<InputAction.CallbackContext> _action) {
