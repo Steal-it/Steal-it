49affae86fea4716700a2ccf0b52242035c265f6
FireStoat3 <161834755+FireStoat3@users.noreply.github.com>
2026-06-30 13:08:11 +0200

    Modified SpectatorMode to work as a plugin. Added sound on player lost

--- Assets/Scripts/Player/SpectatorMode.cs
+++ Assets/Scripts/Player/SpectatorMode.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class SpectatorMode : MonoBehaviour {
@@ -6,16 +7,20 @@ public class SpectatorMode : MonoBehaviour {
 
     private string playerUUID;
 
+    private AudioSource lostAudioSource;
+
     private TorsoIdentifier torso;
 
     void Start() {
         NetworkReferenceManager.Instance.SpectatorModeManager.OnSpectatorModeChange += SpectatorModeManager_OnSpectatorModeChange;
-        playerUUID = gameObject.name;
+        playerUUID = transform.parent.name;
+        lostAudioSource = GetComponent<AudioSource>();
+
         if (playerUUID != "Local Avatar") {
             playerUUID = playerUUID.Split('#')[1];
         }
 
-        torso = gameObject.GetComponentInChildren<TorsoIdentifier>();
+        torso = gameObject.transform.parent.gameObject.GetComponentInChildren<TorsoIdentifier>();
     }
 
     void updateVisibility() {
@@ -28,7 +33,7 @@ public class SpectatorMode : MonoBehaviour {
     }
 
     private void Enable() {
-        gameObject.SetActive(false);
+        gameObject.transform.parent.gameObject.SetActive(false);
 
         //Disable the collider
         if (torso) {
@@ -38,8 +43,8 @@ public class SpectatorMode : MonoBehaviour {
     }
 
     private void Disable() {
-        gameObject.SetActive(true);
-        if (torso) {
+        gameObject.transform.parent.gameObject.SetActive(true);
+        if (torso && playerUUID != "Local Avatar") {
             var col = torso.GetComponent<Collider>();
             col.enabled = true;
         }
@@ -48,10 +53,20 @@ public class SpectatorMode : MonoBehaviour {
     private void SpectatorModeManager_OnSpectatorModeChange(object _sender,
     SpectatorModeManager.OnSpectatorModeChangeEventArgs _args) {
         //Case of an avatar being killed by the monster
+        //The coroutine allow for the sound to play when the Local Avatar is dead, otherwise it will stop since the avatar is deactivated
 
-        if (_args.PlayerUUID == playerUUID || (playerUUID == "Local Avatar" && _args.PlayerUUID == NetworkReferenceManager.Instance.RoomClient.Me.uuid)) {
+        StartCoroutine(HandleSpectatorChange(_args.PlayerUUID));
+    }
+
+    private IEnumerator HandleSpectatorChange(string receivedPlayerUUID) {
+        if (playerUUID == "Local Avatar") {
+            lostAudioSource.Play();
+
+            yield return new WaitUntil(() => !lostAudioSource.isPlaying);
+        }
+
+        if (receivedPlayerUUID == playerUUID || (playerUUID == "Local Avatar" && receivedPlayerUUID == NetworkReferenceManager.Instance.RoomClient.Me.uuid)) {
             updateVisibility();
-            return;
         }
     }
 
